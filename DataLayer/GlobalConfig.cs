5b607c3ceab966f03e963ba9ec6163821bbcff48
Barbora Kovalská <barca.kovalska@gmail.com>
2023-04-28 17:07:04 +0200

    Added GlobalConfig & Logging

---
+++ DataLayer/GlobalConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace DataLayer
+{
+    public class GlobalConfig
+    {
+        public static string ConnectionString { get; set; } = "Data Source=database.db;";
+        public static bool LogConsole { get; set; } = true;
+        public static bool LogFile { get; set; } = false;
+        public static string LogFilePath { get; set; } = "log.txt";
+    }
+}
---
+++ DataLayer/SqlLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace DataLayer
+{
+    public class SqlLog
+    {
+        public static void Log(string type, string sql, Dictionary<string, object> parameters)
+        {
+            if (!GlobalConfig.LogConsole && !GlobalConfig.LogFile)
+                return;
+            
+            foreach (KeyValuePair<string, object> param in parameters)
+            {
+                sql = sql.Replace(param.Key, param.Value.ToString());
+            }
+            
+            if (GlobalConfig.LogConsole)
+            {
+                Console.WriteLine($"Executing {type}:");
+                Console.WriteLine(sql);
+            }
+            if (GlobalConfig.LogFile)
+            {
+                using (StreamWriter file = new StreamWriter(GlobalConfig.LogFilePath, true))
+                {
+                    file.WriteLine($"Executing {type}:");
+                    file.WriteLine(sql);
+                }
+            }
+        }
+    }
+}
