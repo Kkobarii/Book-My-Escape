1ea07a7530aa8d27c22a4197d326e24e58482199
Barbora Kovalská <barca.kovalska@gmail.com>
2023-05-01 12:41:57 +0200

    Added logic for recognizing current directory

--- DataLayer/GlobalConfig.cs
+++ DataLayer/GlobalConfig.cs
@@ -2,9 +2,26 @@
 {
     public class GlobalConfig
     {
-        public static string DatabasePath { get; set; } = "database.db;";
+        public static string AssetsPath 
+        { 
+            get
+            {
+                string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)!;
+
+                path = path.Substring(0, path.IndexOf("SemestralProject\\") + 17);
+                path = path.Substring(6);
+                path += "Assets\\";
+
+                return path;
+            }
+            set
+            {
+                AssetsPath = value;
+            }
+        }
+        public static string DatabasePath { get; set; } = AssetsPath + "database.db";
         public static bool LogConsole { get; set; } = true;
         public static bool LogFile { get; set; } = true;
-        public static string LogFilePath { get; set; } = "log.txt";
+        public static string LogFilePath { get; set; } = AssetsPath + "log.txt";
     }
 }
